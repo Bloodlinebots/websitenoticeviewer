9d11e69e43d1f9853e3049ece92b239a1f51b9ac
Bloodlinebots <githubsbots@gmail.com>
2025-07-27 22:29:18 +0530

    Update db.py

--- db.py
+++ db.py
@@ -10,15 +10,26 @@ def add_user(user_id):
 def get_all_users():
     return [u["_id"] for u in db.users.find()]
 
-def add_site(name, url):
-    db.sites.update_one({"name": name}, {"$set": {"name": name, "url": url}}, upsert=True)
+def add_site_for_user(user_id, name, url):
+    db.sites.update_one(
+        {"user_id": user_id, "name": name},
+        {"$set": {"url": url}},
+        upsert=True
+    )
 
-def get_sites():
+def get_sites_for_user(user_id):
+    return list(db.sites.find({"user_id": user_id}))
+
+def get_all_sites_with_users():
     return list(db.sites.find())
 
-def save_last_notice(site_name, notice_url):
-    db.notices.update_one({"site": site_name}, {"$set": {"url": notice_url}}, upsert=True)
+def save_last_notice(user_id, site_name, notice_url):
+    db.notices.update_one(
+        {"user_id": user_id, "site": site_name},
+        {"$set": {"url": notice_url}},
+        upsert=True
+    )
 
-def get_last_notice(site_name):
-    data = db.notices.find_one({"site": site_name})
-    return data['url'] if data else None
+def get_last_notice(user_id, site_name):
+    doc = db.notices.find_one({"user_id": user_id, "site": site_name})
+    return doc['url'] if doc else None
