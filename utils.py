15be173f6146401cfc80768ee64efef875e4037e
Bloodlinebots <githubsbots@gmail.com>
2025-07-27 22:31:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,16 +3,16 @@ import requests
 
 def download_pdf(url):
     filename = url.split("/")[-1]
-    filepath = f"./downloads/{filename}"
     os.makedirs("downloads", exist_ok=True)
+    filepath = f"./downloads/{filename}"
     with open(filepath, "wb") as f:
         f.write(requests.get(url).content)
     return filepath
 
 def format_notice_message(notice):
     if notice["type"] == "pdf":
-        return {"caption": f"📄 *{notice['title']}*", "parse_mode": "Markdown", "document": open(notice['file'], 'rb')}
+        return {"document": open(notice["file"], "rb"), "caption": f"📄 *{notice['title']}*", "parse_mode": "Markdown"}
     elif notice["type"] == "link":
-        return {"text": f"🔗 *{notice['title']}*\n[Open Notice]({notice['url']})", "parse_mode": "Markdown"}
+        return {"text": f"🔗 *{notice['title']}*\n[Open]({notice['url']})", "parse_mode": "Markdown"}
     else:
         return {"text": f"📢 *{notice['title']}*\n{notice['url']}", "parse_mode": "Markdown"}
